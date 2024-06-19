9d42cf20990187bef60c6783bda0edc10d40de03
Baskoro <139613178+vntion@users.noreply.github.com>
2024-06-19 21:11:48 +0700

    Add files via upload

---
+++ Flazz store project/src/js/login.js
@@ -0,0 +1,13 @@
+const iconEye = document.querySelectorAll('.form__input-icon');
+const inputPassword = document.querySelector('.form__input--password');
+let type = 'password';
+
+const inputTypeToggle = function () {
+  iconEye.forEach(el => el.classList.toggle('hidden'));
+
+  type === 'password' ? (type = 'text') : (type = 'password');
+
+  inputPassword.type = type;
+};
+
+iconEye.forEach(icon => icon.addEventListener('click', inputTypeToggle));
