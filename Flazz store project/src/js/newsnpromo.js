c55ca5d96d3d8badd8ad6535d393f69aa9812482
Trancend1 <farhanalamsyah099@gmail.com>
2024-06-25 17:20:51 +0700

    1
    1

--- Flazz store project/src/js/newsnpromo.js
+++ Flazz store project/src/js/newsnpromo.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
   // Hide all content boxes initially
   $(".content-box").hide();
 
-  // Show the first 4 content boxes
+  // Show the first 6 content boxes
   $(".content-box").slice(0, 6).show();
 
   // Add event listener to the Load More button
-  $("#loadMore").on("click", function (e) {
+  $("#load-more").on("click", function (e) {
     e.preventDefault();
 
-    // Show the next 4 hidden content boxes
-    $(".content-box:hidden").slice(0, 3).slideDown();
+    // Show the next 3 hidden content boxes
+    $(".content-box:hidden").slice(0, 6).slideDown();
 
     // Check if there are no more hidden content boxes
     if ($(".content-box:hidden").length == 0) {
